de9ed7afa92ec74d385c198ce204d7e6efc78ca7
NickNocc <haretakis4@gmail.com>
2021-12-21 15:50:33 -0800

    Add Heroku

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ function filterByQuery(query, animalsArray) {
         // but here we're updating it for each trait in the .forEach() loop.
         // For each trait being targeted by the filter, the filteredResults
         // array will then contain only the entries that contain the trait,
+        
         // so at the end we'll have an array of animals that have every one 
         // of the traits when the .forEach() loop is finished.
         filteredResults = filteredResults.filter(
